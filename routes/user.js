49b5033fe9f16736898abf5848f95e595c53da41
Alessandro Caprarelli <alessandro.caprarelli.ext@siemens.com>
2016-12-11 12:38:21 +0100

    Fixed typo in routes

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('user-login', function (req, res) {
+router.get('/user-login', function (req, res) {
     res.render('user-login');
 });
 
-router.get('user-show-qr', function (req, res) {
+router.get('/user-show-qr', function (req, res) {
     
 });
 
