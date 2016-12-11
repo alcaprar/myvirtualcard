cc81ab91e28f0047c1fbc872dfa78834dc733a73
Alessandro Caprarelli <alessandro.caprarelli.ext@siemens.com>
2016-12-11 12:30:34 +0100

    Fixed bootstrap dependencies

--- routes/seller.js
+++ routes/seller.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/seller-login', function (req, res) {
-    
+    res.render('seller-login');
 });
 
 router.get('/seller-scan-qr', function (req, res) {
